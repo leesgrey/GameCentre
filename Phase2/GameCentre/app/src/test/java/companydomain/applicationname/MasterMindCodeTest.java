6f2389dc099830625a88cd14b4d51541b9b8286e
Andrew Lee <wtfdudeyay@gmail.com>
2018-11-20 16:00:09 -0500

    categorized the MasterMindCode tests by method name and added docstrings.

--- Phase2/GameCentre/app/src/test/java/companydomain/applicationname/MasterMindCodeTest.java
+++ Phase2/GameCentre/app/src/test/java/companydomain/applicationname/MasterMindCodeTest.java
@@ -6,10 +6,23 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MasterMindCodeTest {
+    /**
+     * The MasterMindCode answer.
+     */
     private MasterMindCode answer;
+    /**
+     * The first guessed MasterMindCode.
+     */
     private MasterMindCode guess1;
+    /**
+     * The second guessed MasterMindCode.
+     */
     private MasterMindCode guess2;
 
+
+    /**
+     * Initializes the MasterMinCode instances, i.e. answer, guess1, and guess2.
+     */
     @Before
     public void setUp(){
         answer = new MasterMindCode(new int[] {4, 1, 1, 6, 5, 5});
@@ -17,19 +30,40 @@ public class MasterMindCodeTest {
         guess2 = new MasterMindCode(new int[] {3, 1, 4, 5, 3, 2}, answer);
     }
 
-    public MasterMindCodeTest() {
+
+    /**
+     * Test the getCode() method.
+     */
+
+    @Test
+    public void getCode() {
         setUp();
         assertArrayEquals(new int[] {4, 1, 1, 6, 5, 5}, answer.getCode());
         assertArrayEquals(new int[] {1, 1, 1, 1, 1, 1}, guess1.getCode());
         assertArrayEquals(new int[] {3, 1, 4, 5, 3, 2}, guess2.getCode());
+
+
+    }
+
+    /**
+     * Test the getCorectness() method.
+     */
+    @Test
+    public void getCorrectness() {
+        setUp();
         assertArrayEquals(new int[] {6, 0}, answer.getCorrectness());
         assertArrayEquals(new int[] {2, 0}, guess1.getCorrectness());
         assertArrayEquals(new int[] {1, 2}, guess2.getCorrectness());
+
     }
 
-    // TODO: figure out why this isn't working, cause the equals method seems to work
+
+    /**
+     * Test comparison method of MasterMindCode.
+     */
+
     @Test
-    public void equalsTest() {
+    public void equals() {
         setUp();
         assertEquals(answer,new MasterMindCode(new int[] {4, 1, 1, 6, 5, 5}));
         assertEquals(guess1, new MasterMindCode(new int[] {1, 1, 1, 1, 1, 1}));
