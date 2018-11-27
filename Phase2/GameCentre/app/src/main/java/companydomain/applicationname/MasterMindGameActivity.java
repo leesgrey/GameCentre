822e31961a5ef70916decd16cd3116a34c03f970
Jing Hon <jing.hon@mail.utoronto.ca>
2018-11-27 18:41:56 -0500

    MasterMindGameActivity UI updated, fixed code smells and made the game work better. TODO: finish off the game and ensures that it terminates properly and then passes on a scoreboard properly

--- Phase2/GameCentre/app/src/main/java/companydomain/applicationname/MasterMindGameActivity.java
+++ Phase2/GameCentre/app/src/main/java/companydomain/applicationname/MasterMindGameActivity.java
@@ -11,82 +11,96 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MasterMindGameActivity extends AppCompatActivity {
+    private MasterMindManager manager;
     private String currentUser;
     private int size;
     private Spinner singleEntryGuess;
     private int inputCounter = 1;
+    private int[] answer;
+    private TextView text1, text2, text3, text4, text5, text6, text7, text8, text9, text10,
+            guess1, guess2, guess3, guess4, guess5, correct1, correct2, correct3, correct4, score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mastermindgame);
 
+        //Receiving the values passed from the previous frame
         Intent i = getIntent();
         this.currentUser = i.getStringExtra("currentUser");
         TextView currUser = findViewById(R.id.currentuserText);
         currUser.setText(String.format("Player: %s", currentUser));
-
         this.size = i.getIntExtra("size", 5);
 
-        setGameVisual();
-
+        //Setting the spinner
         this.singleEntryGuess = (Spinner) findViewById(R.id.singleEntryGuess);
-
-
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.possible_inputs
                 , android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         singleEntryGuess.setAdapter(adapter);
 
+        //Setting the GUI for the game
+        setFields();
+        setGameVisual();
         inputListener();
         submitGuessListener();
         undoListener();
+        clearListener();
 
-        MasterMindManager manager = new MasterMindManager(this.size, 5);
-
-
+        //Generating the game itself
+        this.manager = new MasterMindManager(this.size, 5);
+        this.answer = new int[this.size];
 
     }
 
-    //TODO: remove code smells and preventing from doing this inefficiently
+    /**
+     * Quick setting of the fields so that the display can be updated easily
+     */
+    void setFields() {
+        this.text1 = findViewById(R.id.firstEntryOne);
+        this.text2 = findViewById(R.id.firstEntryTwo);
+        this.text3 = findViewById(R.id.firstEntryThree);
+        this.text4 = findViewById(R.id.firstEntryFour);
+        this.text5 = findViewById(R.id.firstEntryFive);
+        this.text6 = findViewById(R.id.secondEntryOne);
+        this.text7 = findViewById(R.id.secondEntryTwo);
+        this.text8 = findViewById(R.id.secondEntryThree);
+        this.text9 = findViewById(R.id.secondEntryFour);
+        this.text10 = findViewById(R.id.secondEntryFive);
+        this.guess1 = findViewById(R.id.guess1);
+        this.guess2 = findViewById(R.id.guess2);
+        this.guess3 = findViewById(R.id.guess3);
+        this.guess4 = findViewById(R.id.guess4);
+        this.guess5 = findViewById(R.id.guess5);
+        this.correct1 = findViewById(R.id.firstCorrectOne);
+        this.correct2 = findViewById(R.id.firstCorrectTwo);
+        this.correct3 = findViewById(R.id.secondCorrectOne);
+        this.correct4 = findViewById(R.id.secondCorrectTwo);
+        this.score = findViewById(R.id.score);
+    }
+
+    /***
+     * Method that sets the visual of the game depending on the game size indicated from the menu
+     */
     void setGameVisual() {
         if (this.size == 3) {
-            TextView text = findViewById(R.id.firstEntryFour);
-            text.setText("");
-            TextView text1 = findViewById(R.id.firstEntryFive);
-            text1.setText("");
-            TextView text2 = findViewById(R.id.secondEntryFour);
-            text2.setText("");
-            TextView text3 = findViewById(R.id.secondEntryFive);
-            text3.setText("");
-            TextView text4 = findViewById(R.id.thirdEntryFour);
-            text4.setText("");
-            TextView text5 = findViewById(R.id.thirdEntryFive);
-            text5.setText("");
-            TextView text6 = findViewById(R.id.fourthEntryFour);
-            text6.setText("");
-            TextView text7 = findViewById(R.id.fourthEntryFive);
-            text7.setText("");
-            TextView text8 = findViewById(R.id.fifthEntryFour);
-            text8.setText("");
-            TextView text9 = findViewById(R.id.fifthEntryFive);
-            text9.setText("");
+            this.text4.setText("");
+            this.text5.setText("");
+            this.text9.setText("");
+            this.text10.setText("");
         } else if (this.size == 4) {
-            TextView text1 = findViewById(R.id.firstEntryFive);
-            text1.setText("");
-            TextView text3 = findViewById(R.id.secondEntryFive);
-            text3.setText("");
-            TextView text5 = findViewById(R.id.thirdEntryFive);
-            text5.setText("");
-            TextView text7 = findViewById(R.id.fourthEntryFive);
-            text7.setText("");
-            TextView text9 = findViewById(R.id.fifthEntryFive);
-            text9.setText("");
+            this.text5.setText("");
+            this.text10.setText("");
         }
     }
 
+    /**
+     * Listener that keeps track of the input
+     */
     void inputListener(){
         Button inputButton = findViewById(R.id.inputEntryButton);
         inputButton.setOnClickListener(new View.OnClickListener() {
@@ -100,27 +114,20 @@ public class MasterMindGameActivity extends AppCompatActivity {
 
     void changeText(String text){
         if (this.inputCounter == 1){
-            TextView guess1 = findViewById(R.id.guess1);
-            guess1.setText(text);
+            this.guess1.setText(text); this.answer[this.inputCounter - 1] = Integer.parseInt(text);
             this.inputCounter += 1;
         } else if (this.inputCounter == 2) {
-            TextView guess2 = findViewById(R.id.guess2);
-            guess2.setText(text);
+            this.guess2.setText(text); this.answer[this.inputCounter - 1] = Integer.parseInt(text);
             this.inputCounter += 1;
         } else if (this.inputCounter == 3) {
-            TextView guess3 = findViewById(R.id.guess3);
-            guess3.setText(text);
+            this.guess3.setText(text); this.answer[this.inputCounter - 1] = Integer.parseInt(text);
             this.inputCounter += 1;
-        } else if (this.inputCounter == 4) {
-            TextView guess4 = findViewById(R.id.guess4);
-            guess4.setText(text);
+        } else if (this.inputCounter == 4 && this.size > 3) {
+            this.guess4.setText(text); this.answer[this.inputCounter - 1] = Integer.parseInt(text);
             this.inputCounter += 1;
-        } else if (this.inputCounter == 5) {
-            TextView guess5 = findViewById(R.id.guess5);
-            guess5.setText(text);
+        } else if (this.inputCounter == 5 && this.size > 4) {
+            this.guess5.setText(text); this.answer[this.inputCounter - 1] = Integer.parseInt(text);
             this.inputCounter += 1;
-        } else{
-            return;
         }
     }
 
@@ -129,9 +136,58 @@ public class MasterMindGameActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: take the guesses shown on the code and implementing it further
+                submitAnswer();
             }
         });
+    }
+
+    void submitAnswer(){
+        this.manager.makeGuess(this.answer);
+        resetText();
+        displayResult();
+        this.guess1.setText("");
+        this.guess2.setText("");
+        this.guess3.setText("");
+        this.guess4.setText("");
+        this.guess5.setText("");
+    }
+
+    void displayResult(){
+        MasterMindCombination[] lastNGuesses = this.manager.getLastNGuesses(2);
+        this.score.setText(String.valueOf(this.manager.getScore()));
+        if (this.size == 3){
+            displayThree(lastNGuesses);
+        } else if (this.size == 4){
+            displayFour(lastNGuesses);
+        } else{
+            displayFive(lastNGuesses);
+        }
+    }
+
+    void displayThree(MasterMindCombination[] lastNGuesses){
+        int[] secondLast = lastNGuesses[1].getCode();
+        int[] secondCorrect = lastNGuesses[1].getCorrectness();
+        this.text1.setText(String.valueOf(secondLast[0]));
+        this.text2.setText(String.valueOf(secondLast[1]));
+        this.text3.setText(String.valueOf(secondLast[2]));
+        this.correct1.setText(String.valueOf(secondCorrect[0]));
+        this.correct2.setText(String.valueOf(secondCorrect[1]));
+        int[] last = lastNGuesses[0].getCode();
+        int[] lastCorrect = lastNGuesses[0].getCorrectness();
+        this.text6.setText(String.valueOf(last[0]));
+        this.text7.setText(String.valueOf(last[1]));
+        this.text8.setText(String.valueOf(last[2]));
+        this.correct1.setText(String.valueOf(lastCorrect[0]));
+        this.correct2.setText(String.valueOf(lastCorrect[1]));
+
+    }
+
+    void displayFour(MasterMindCombination[] lastNGuesses){
+    }
+
+    void displayFive(MasterMindCombination[] lastNGuesses){
+
+
     }
 
     void undoListener(){
@@ -142,5 +198,22 @@ public class MasterMindGameActivity extends AppCompatActivity {
                 //TODO: take the corresponding value and append to the guesses
             }
         });
+    }
+
+    void clearListener(){
+        Button clearButton = findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetText();
+            }
+        });
+    }
+
+    void resetText(){
+        this.guess1.setText(""); this.guess2.setText(""); this.guess3.setText("");
+        this.guess4.setText(""); this.guess5.setText("");
+        this.inputCounter = 1;
+        this.answer = new int[this.size];
     }
 }
