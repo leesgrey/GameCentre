e7378e46d47bcaf62bf17256bf665e9923a649d0
Hugh Goatcher <hugh.goatcher@mail.utoronto.ca>
2018-11-30 02:42:48 -0500

    Rewrote MBMTest class

--- Phase2/GameCentre/app/src/test/java/companydomain/applicationname/MatchingBoardManagerTest.java
+++ Phase2/GameCentre/app/src/test/java/companydomain/applicationname/MatchingBoardManagerTest.java
@@ -1,125 +1,77 @@
 package companydomain.applicationname;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
+import java.util.Collections;
 import java.util.List;
-
 
 import static org.junit.Assert.*;
 
 public class MatchingBoardManagerTest {
 
-    private MatchingBoardManager matchingBoardManager;
-    private int numRows;
-    private int numCols;
-    List<Tile> unMatchedTiles;
-    List<Tile> selectedTiles;
-
     /**
-     * Initialize the MatchingBoardManager instance.
-     *
-     * @param numRows the number of rows
-     * @param numCols the number of columns
+     * A MatchingBoardManager to test.
      */
-    private void setupMatchingBoard(int numRows, int numCols) {
-        this.numRows = numRows;
-        this.numCols = numCols;
-        this.matchingBoardManager = new MatchingBoardManager(numRows, numCols);
+    MatchingBoardManager matchingBoardManager;
+    @Before
+    public void setUp() {
+        matchingBoardManager = new MatchingBoardManager(4, 5);
+        matchingBoardManager.getBoard().sortTilesWithZeroLast();
     }
 
-
-    private void createUnmatchedandSelectedTiles()
-    {   selectedTiles = new ArrayList<>();
-        unMatchedTiles = new ArrayList<>();
-        int numTiles = numRows * numCols;
-        for (int tileNum = 0; tileNum != numTiles/2; tileNum++) {
-            Tile firstOfPair = new Tile(tileNum + 1);
-            Tile secondOfPair = new Tile(tileNum + 1);
-            unMatchedTiles.add(firstOfPair);
-            unMatchedTiles.add(secondOfPair);
-        }
-    }
-
-/**
- * Test isValidTap() method.
- */
     @Test
-    public void isValidTapTest() {
-        setupMatchingBoard(3,4);
-        boolean testtap1 = matchingBoardManager.isValidTap(5);
-
-        assertEquals(true, testtap1);
-
-        List<Tile> selected = matchingBoardManager.getSelected();
+    public void getUnmatched() {
         List<Tile> unmatched = matchingBoardManager.getUnmatched();
-        Tile id = matchingBoardManager.board.getTile(5);
-
-        selected.add(id);
-        boolean testtap2 = matchingBoardManager.isValidTap(5);
-        assertEquals(false, testtap2);
-
-        unmatched.remove(id);
-        boolean testtap3 = matchingBoardManager.isValidTap(5);
-        assertEquals(false, testtap3);
-
+        Collections.sort(unmatched);
+        for(int i = 0; i < 20; i++) {
+            assertEquals(unmatched.get(i).getId(), (i / 2) + 1);
         }
-
-    /**
-     * Test touchMove() method.
-     */
-    @Test
-    public void touchMoveTest() {
-
-        setupMatchingBoard(3,4);
-        createUnmatchedandSelectedTiles();
-
-        List<Tile> unMatchedTest = this.unMatchedTiles;
-
-        List<Tile> selectedActual = matchingBoardManager.getSelected();
-        List<Tile> unMatchedActual = matchingBoardManager.getUnmatched();
-
-
-        matchingBoardManager.board.sortTilesWithZeroLast();
-
-        Tile id = matchingBoardManager.board.getTile(3);
-
         matchingBoardManager.touchMove(0);
         matchingBoardManager.touchMove(1);
-
-        assertEquals(unMatchedTest.size()-2,  unMatchedActual.size());
-
-
-        matchingBoardManager.touchMove(5);
-        assertEquals(1, selectedActual.size());
-
-        matchingBoardManager.touchMove(5);
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for(int i = 0; i < 18; i++) {
+            assertEquals(unmatched.get(i).getId(), (i / 2) + 2);
         }
-        assertEquals(0, selectedActual.size());
-
-
     }
 
-    /**
-     * Test puzzleSolved() method.
-     */
     @Test
-    public void puzzleSolvedTest() {
-        setupMatchingBoard(3, 4);
-        assertFalse(matchingBoardManager.puzzleSolved());
-        matchingBoardManager.board.sortTilesWithZeroLast();
+    public void getSelected() {
+        assertEquals(matchingBoardManager.getSelected().size(), 0);
+        matchingBoardManager.touchMove(2);
+        assertEquals(matchingBoardManager.getSelected().size(), 1);
+        assertEquals(matchingBoardManager.getSelected().get(0), matchingBoardManager.getBoard().getTile(2));
+    }
 
-        System.out.println();
-        for (int i = 0; i < matchingBoardManager.board.numTiles() ; i++) {
+    @Test
+    public void isValidTap() {
+        assertTrue(matchingBoardManager.isValidTap(0));
+        matchingBoardManager.touchMove(0);
+        assertFalse(matchingBoardManager.isValidTap(0));
+        matchingBoardManager.touchMove(1);
+        assertFalse(matchingBoardManager.isValidTap(0));
+        matchingBoardManager.touchMove(5);
+        assertFalse(matchingBoardManager.isValidTap(0));
+    }
+
+    @Test
+    public void puzzleSolved() {
+        for(int i = 0; i < 20; i++) {
+            assertFalse(matchingBoardManager.puzzleSolved());
             matchingBoardManager.touchMove(i);
-            matchingBoardManager.touchMove(i);
-            }
+        }
         assertTrue(matchingBoardManager.puzzleSolved());
     }
 
+    @Test
+    public void getScoreCounter() {
+        for(int i = 0; i < 20; i++) {
+            matchingBoardManager.touchMove(i);
+            assertEquals(matchingBoardManager.getScoreCounter(), (i + 1) / 2);
+        }
     }
+
+    @Test
+    public void getSize() {
+        assertEquals(matchingBoardManager.getSize(), 5);
+    }
+}
